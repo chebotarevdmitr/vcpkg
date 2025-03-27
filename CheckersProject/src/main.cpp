dcf436b5ce482974b2ae1c503ad0203c7e3f600e
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-03-27 23:47:18 +0300

    Add CMakeLists.txt and main.cpp for CheckersProject

---
+++ CheckersProject/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Для использования std::cout
+
+int main() {
+    // Выводим сообщение в консоль
+    std::cout << "Hello, World! Welcome to the Checkers Game!" << std::endl;
+
+    return 0; // Успешное завершение программы
+}
